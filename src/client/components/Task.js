ad6cdd6d5b1ddb2d8eebda7f61c9aab893d6ee46
Marko Nikolaš <marko.nikolas@protonmail.com>
2021-02-16 14:48:06 -0600

    added custom checkbox

--- src/client/components/Task.js
+++ src/client/components/Task.js
@@ -1,33 +1,55 @@
 import React from 'react';
 import styled from '@emotion/styled'
 
+import check from '../assets/icons/check-bold.svg'
+
 const TaskWrapper = styled.section({ 
 	boxShadow: '1px 1px 5px #C0C0C0',
-	height: '50px',
+	height: '52px',
 	width: '100%',
 	background: '#FFFFFF',
 	boxShadow: '1px 2px 8px rgba(192, 192, 192, 0.5)',
 	display: 'flex',
-	paddingRight: '20px'
 })
 
-const TaskName = styled.label({ 
-	fontWeight: 300,
-	fontSize: '16px',
-	lineHeight: '22px',
-	color: '#8D8D8D',
-	flexGrow: 1,
-	cursor: 'pointer',
-	padding: '15px 20px',
-	userSelect: 'none'
-})
+const TaskName = styled.label`
+	position: relative;
+	font-weight: 300;
+	font-size: 17px;
+	line-height: 22px;
+	color: #8D8D8D;
+	flex: 1;
+	cursor: pointer;
+	padding: 15px 20px;
+	userSelect: none;
+
+	&::after { 
+		content: '';
+		display: block;
+		width: 20px;
+		height: 20px;
+		position: absolute;
+		right: 20px;
+		border: 1px solid #c4c4c4;
+		border-radius: 5px;
+		top: 50%;
+		transform: translateY(-50%)
+	}
+`
+
+const Checkmark = styled.input`
+	&:checked ~ label::after {
+		background: url(${check}) no-repeat center center / contain;
+	}
+`
+
 
 const Task = ({ inputNo }) => {
 	return ( 
 		<TaskWrapper>
+			{/* add custom id generation to avoid bugs on removing tasks */}
+			<Checkmark type="checkbox" name="check" id={`task-${inputNo}`} hidden />
 			<TaskName htmlFor={`task-${inputNo}`}>Lorem, ipsum dolor.</TaskName>
-			{/* checkbox, create custom checkbox and hide other one*/} 
-			<input type="checkbox" name="check" id={`task-${inputNo}`}/>
 		</TaskWrapper>
 	 );
 }
